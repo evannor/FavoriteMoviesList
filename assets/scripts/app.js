4067323c27cdc7fad2706b1fb40c722f211f56dd
evannor <erica.vannorman@gmail.com>
2021-01-08 19:43:35 -0800

    Insert backdrop on modal appearance

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,9 @@
+const toggleBackdrop = () => {
+  document.getElementById("backdrop").classList.toggle('visible');
+}
+
 document.querySelector('header button').addEventListener('click', () => {
   // document.getElementById("add-modal").className = ".modal.visible";
   document.getElementById("add-modal").classList.toggle('visible');
+  toggleBackdrop();
 });
